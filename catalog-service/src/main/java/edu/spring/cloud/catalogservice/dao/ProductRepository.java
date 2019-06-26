f2eba981fe85d62d15cea652a7a1ba2a3300de1e
asokolov <wedmeed@mail.ru>
2019-06-26 18:52:32 +0400

    add business code

---
+++ catalog-service/src/main/java/edu/spring/cloud/catalogservice/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package edu.spring.cloud.catalogservice.dao;
+
+import edu.spring.cloud.catalogservice.dao.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByCode(String code);
+}
